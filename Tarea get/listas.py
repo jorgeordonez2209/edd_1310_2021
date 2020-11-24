597a70b6746a6e0c7b4f0c6f3a3352651691cc9d
jorgeordonez2209 <60630341+jorgeordonez2209@users.noreply.github.com>
2020-11-23 19:07:31 -0600

    Add files via upload

---
+++ Tarea get/listas.py
@@ -0,0 +1,72 @@
+class Nodo:
+    def __init__(self, value, siguiente=None):
+        self.data=value #sin encapsular
+        self.siguiente=siguiente
+
+class LinkedList:
+    def __init__(self):
+        self.__head=None
+
+    def is_empty(self):
+        return self.__head==None
+
+    def append(self, value):
+        nuevo=Nodo(value)
+        if self.__head==None: # o tambien self.__is_empty()
+            self.__head=nuevo
+        else:
+            curr_node=self.__head #poner curr_node al final de los nodos
+            while curr_node.siguiente != None: #poner curr_node al final de los nodos
+                curr_node=curr_node.siguiente #poner curr_node al final de los nodos
+            curr_node.siguiente=nuevo
+
+    def transversal(self):
+        curr_node=self.__head
+        while curr_node!=None:
+            print(f"{curr_node.data}--> ", end="")
+            curr_node=curr_node.siguiente
+        print(" ")
+# curr_node ->>>> todo el nodo #curr_node.data-->>>> dato que hay en el nodo
+
+    def tail(self):
+        curr_node=self.__head
+        while curr_node.siguiente!=None:
+            curr_node=curr_node.siguiente
+        return curr_node
+
+    def remove(self, value):
+        curr_node=self.__head
+        if self.__head.data==value: #para poder eliminar primer nodo
+            self.__head=self.__head.siguiente
+        else:
+            anterior=None
+            while curr_node.data != value and curr_node.siguiente != None: #and--->tomar en cuenta las dos
+                anterior=curr_node
+                curr_node=curr_node.siguiente
+            if curr_node.data == value:
+                anterior.siguiente=curr_node.siguiente
+            else:
+                print("El dato no existe en la lista")
+
+    def preppend(self, value):
+        nuevo=Nodo(value, self.__head)
+        self.__head=nuevo
+
+    def get(self, posicion=None): #por defecto regresa el ultimo
+        #contador=0
+        dato=None
+        if posicion==None:
+            dato=self.tail().data
+        else:
+            if self.__head.data==posicion:
+                dato=self.__head.data
+            else:
+                contador=0
+                while self.__head.data != posicion and self.__head.siguiente != None:
+                    contador=self.__head
+                    self.__head=self.__head.siguiente
+                if self.__head.data==posicion:
+                    dato=self.__head.data
+                else:
+                    print("No exite valor")
+        return dato
